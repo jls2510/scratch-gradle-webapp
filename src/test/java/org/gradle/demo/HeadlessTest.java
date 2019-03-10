d06b1c7bcec9c42f7c769d2c693f7d6454fc6f0b
James L. Stevenson <jim.stevenson@kogentservices.com>
2019-03-10 11:23:38 -0400

    Working on Selenium testing.

--- src/test/java/org/gradle/demo/HeadlessTest.java
+++ src/test/java/org/gradle/demo/HeadlessTest.java
@@ -1,40 +1,32 @@
 package org.gradle.demo;
 
 import io.github.bonigarcia.wdm.ChromeDriverManager;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.io.IOException;
+
 import static org.junit.Assert.assertEquals;
 
-public class HelloServletFunctionalTest {
-    private WebDriver driver;
+
+public class HeadlessTest {
 
     @BeforeClass
     public static void setupClass() {
-        ChromeDriverManager.getInstance().setup(); 
-    }
-
-    @Before
-    public void setUp() {
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--headless");
-        driver = new ChromeDriver(chromeOptions);
-    }
-
-    @After
-    public void tearDown() {
-        if (driver != null)
-            driver.quit();                         
+        ChromeDriverManager.getInstance().setup();
     }
 
     @Test
-    public void sayHello() throws Exception {      
+    public void HeadlessChromeDriverTest() throws IOException {
+
+        //System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver/chromedriver.exe");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless");
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
         driver.get("http://localhost:8081/webdemo");
 
         driver.findElement(By.id("say-hello-text-input")).sendKeys("Dolly");
@@ -42,5 +34,7 @@ public class HelloServletFunctionalTest {
 
         assertEquals("Hello Page", driver.getTitle());
         assertEquals("Hello, Dolly!", driver.findElement(By.tagName("h2")).getText());
+
+        driver.quit();
     }
 }
